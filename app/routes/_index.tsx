d8e5a3c2c9bf5c4316f1e4d66286bb9ef32f0aaf
koobawuah <me@bawuahboakye.com>
2025-04-15 14:45:23 +0000

    chore: quick laps x2

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -71,8 +71,8 @@ export default function Index() {
 export function PrimaryChips({ text }: { text: string }) {
   return (
     <div className="max-w-max flex items-center gap-x-1.5 py-[6px] px-[10px] border border-primary rounded-full ">
-      <span className="size-1 bg-primary rounded-full animate-pulse" />
-      <p className=" text-primary font-normal uppercase text-sm ">{text}</p>
+      <span className="size-1 bg-primary rounded-full animate-pulse-2x" />
+      <p className=" text-primary font-medium uppercase text-xs">{text}</p>
     </div>
   );
 }
